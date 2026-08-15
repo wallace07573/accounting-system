4ff28433dcd1d16ffbfd1e90f5a55a095134d88b
wallace07573 <wallacekok07573@gmail.com>
2026-08-15 20:08:39 +0800

    fix: update documents type constraint for Pre-Order

--- migrate_to_preorder.sql
+++ migrate_to_preorder.sql
@@ -1,4 +1,11 @@
--- Update all Invoices to Pre-Order EXCEPT those belonging to a group named 'Sales Invoice'
+-- 1. Drop the old constraint
+ALTER TABLE documents DROP CONSTRAINT IF EXISTS documents_type_check;
+
+-- 2. Add the new constraint with 'Pre-Order' included
+ALTER TABLE documents ADD CONSTRAINT documents_type_check 
+CHECK (type IN ('Invoice', 'Quotation', 'Delivery Order', 'Pre-Order'));
+
+-- 3. Update all Invoices to Pre-Order EXCEPT those belonging to a group named 'Sales Invoice'
 UPDATE documents 
 SET type = 'Pre-Order' 
 WHERE type = 'Invoice' 
